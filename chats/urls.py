91e5cb3885b8d8d4f08c644e9bb831275d7b59d7
egor187 <py.egor.py@gmail.com>
2021-06-16 22:15:54 +0300

    add namespace settings for apps urls.py

--- chats/urls.py
+++ chats/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from chats.views import ChatDetailView
 
+app_name = "chats"
+
 urlpatterns = [
     path("<int:pk>/", ChatDetailView.as_view(), name="chat_detail"),
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from users.views import UserDetailView
 
+app_name = "users"
+
 urlpatterns = [
     path("<int:pk>/", UserDetailView.as_view(), name="user_detail"),
 ]
