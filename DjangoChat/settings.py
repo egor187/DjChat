1473178825318f743b76e856d5ddab353d4908c9
egor187 <py.egor.py@gmail.com>
2021-06-16 22:08:16 +0300

    add django-channels for WebSocket chat realization and plug it ti Django

--- DjangoChat/settings.py
+++ DjangoChat/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'channels',
+
     'users',
     'chats',
 ]
@@ -135,3 +137,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = "DjangoChat.asgi.application"
