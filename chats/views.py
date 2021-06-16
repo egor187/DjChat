ff6ac68ac05080c5ef7cd5e73b45d3779b0b3a84
egor187 <py.egor.py@gmail.com>
2021-06-16 23:01:06 +0300

    refactor context data for ChatDetailView CBV get opportunity for serializing Django objects for JS

--- chats/views.py
+++ chats/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import DetailView
+from django.core import serializers
 from .models import Chat
 
 
@@ -6,3 +7,8 @@ class ChatDetailView(DetailView):
     model = Chat
     template_name = "chat_detail.html"
     context_object_name = "chat"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["chat"] = serializers.serialize("json", Chat.objects.filter(pk=self.kwargs["pk"]))
+        return context
