521c2ae2d2cb0489a88223078a86e9b3a5b00762
egor187 <py.egor.py@gmail.com>
2021-06-16 14:36:33 +0300

    refactor: add methods to "MyUser" model for getting related "Chat" instances

--- users/models.py
+++ users/models.py
@@ -1,5 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 
+from chats.models import Chat
+
 
 class MyUser(AbstractUser):
-    pass
+    def get_chats_created(self):
+        """
+        :return: queryset of 'Chat' instances in which 'self' user related like 'creator'
+        """
+        chats = Chat.objects.filter(creator=self)
+        return chats
+
+    def get_chats_membership(self):
+        """
+        :return: queryset of 'Chat' instances in which 'self' user related like 'member'
+        """
+        chats = Chat.objects.filter(members=self)
+        return chats
