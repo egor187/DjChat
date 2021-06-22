c4ab9d5cc03c34d0483126645510fa91d814b446
egor187 <py.egor.py@gmail.com>
2021-06-22 16:42:29 +0300

    plug LoginRequiredMixin to UserDetail CBV

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
+
 from users.models import MyUser
 
 
-class UserDetailView(DetailView):
+class UserDetailView(LoginRequiredMixin, DetailView):
     model = MyUser
     template_name = 'user_detail.html'
     context_object_name = "user"
