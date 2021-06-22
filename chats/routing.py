fed9fb549ba767b6e14bc79f27a6e315150233e9
egor187 <py.egor.py@gmail.com>
2021-06-22 10:43:45 +0300

    set routing for ws-connections

---
+++ chats/routing.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from chats.consumers import ChatConsumer
+
+
+websocket_urlpatterns = [
+    path("ws/chat/<int:pk>/", ChatConsumer.as_asgi(), name="ws_chat_detail"),
+]
