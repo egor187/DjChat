fd073156280bfc23e51cd18259f457a42e7019e8
egor187 <py.egor.py@gmail.com>
2021-06-22 10:42:53 +0300

    plug "index" page and Django built-in auth urls to URLConf

--- DjangoChat/urls.py
+++ DjangoChat/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import index
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', index, name="index"),
     path('users/', include('users.urls')),
     path('chats/', include('chats.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
