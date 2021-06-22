13ac82ae58e7ce01802b1252519fb857cce5a3dd
egor187 <py.egor.py@gmail.com>
2021-06-22 10:45:01 +0300

    add new context from ChatDetail CBV - all messages related to self.chat instance

--- chats/views.py
+++ chats/views.py
@@ -1,14 +1,20 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
 from django.core import serializers
-from .models import Chat
+
+from .models import Chat, Message
 
 
-class ChatDetailView(DetailView):
+class ChatDetailView(LoginRequiredMixin, DetailView):
     model = Chat
     template_name = "chat_detail.html"
     context_object_name = "chat"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context["chat"] = serializers.serialize("json", Chat.objects.filter(pk=self.kwargs["pk"]))
+        context["chat_pk"] = Chat.objects.get(pk=self.kwargs["pk"]).pk
+
+        # context["all_messages"] = serializers.serialize("json", Message.objects.filter(chat=self.object), fields="text")
+        context["all"] = Message.objects.filter(chat=self.object).values_list("text", flat=True)
+
         return context
