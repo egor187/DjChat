8f0109c12d9c08aae9744dac957cc8821eb6c376
egor187 <py.egor.py@gmail.com>
2021-06-22 16:41:55 +0300

    add ChartCreate CBV

--- chats/views.py
+++ chats/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import DetailView, CreateView
 from django.core import serializers
 
+from .forms import ChatCreateForm
 from .models import Chat, Message
 
 
@@ -18,3 +21,35 @@ class ChatDetailView(LoginRequiredMixin, DetailView):
         context["all"] = Message.objects.filter(chat=self.object).values_list("text", flat=True)
 
         return context
+
+
+class ChatCreateView(LoginRequiredMixin, CreateView):
+    model = Chat
+    form_class = ChatCreateForm
+    template_name  = "chat_create.html"
+
+    def get(self, request, *args, **kwargs):
+        form = ChatCreateForm()
+        form.fields["members"].queryset = form.fields["members"].queryset.exclude(
+            pk=self.request.user.pk
+        )
+        return render(self.request, self.template_name, {'form': form})
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        super().form_valid(form)
+
+        chat = Chat.objects.get(pk=form.instance.pk)
+        chat.members.add(self.request.user.pk)
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    # Alternative realization
+    # def form_valid(self, form):
+    #     new_chat_bounded_form = form.save(commit=False)
+    #     new_chat_bounded_form.creator = self.request.user
+    #     super().form_valid(form)
+    #
+    #     chat = Chat.objects.get(pk=form.instance.pk)
+    #     chat.members.add(self.request.user.pk)
+    #     return HttpResponseRedirect(self.get_success_url())
