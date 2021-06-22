6bc8932395751e025b9a42fabd039c8912c0c458
egor187 <py.egor.py@gmail.com>
2021-06-22 10:38:25 +0300

    add Django session middleware to websocket router

--- DjangoChat/asgi.py
+++ DjangoChat/asgi.py
@@ -11,12 +11,15 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chats.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChat.settings')
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(chats.routing.websocket_urlpatterns)),
     }
 )
