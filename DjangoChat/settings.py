71e207cc7b33b9580b2d284a02a5c39962f73ad3
egor187 <py.egor.py@gmail.com>
2021-06-24 17:45:04 +0300

    deploy: set environ variables

--- DjangoChat/settings.py
+++ DjangoChat/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'DjangoChat.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Chat_db',
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '5432',
     }
 }
