4c4fba2c0d3b590247e52a32bc069bf38f2cecf6
egor187 <py.egor.py@gmail.com>
2021-06-24 22:46:28 +0300

    deploy: refactor imports to load first get_asgi_application and second - AuthMiddlewareStack. Thus, middleware will have access only after instance of asge app will be created

--- DjangoChat/asgi.py
+++ DjangoChat/asgi.py
@@ -11,15 +11,16 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChat.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chats.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChat.settings')
-
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(URLRouter(chats.routing.websocket_urlpatterns)),
     }
 )
